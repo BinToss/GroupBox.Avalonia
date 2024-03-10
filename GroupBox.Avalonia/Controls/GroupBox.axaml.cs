7647202dfa3ffee428a8e47dfd7debf96085d5f9
Noah Sherwin <noahrsherwin@gmail.com>
2024-03-10 06:35:48 -0700

    refactor(GroupBox.Avalonia): simplify class definition; add xmldoc

--- GroupBox.Avalonia/Controls/GroupBox.axaml.cs
+++ GroupBox.Avalonia/Controls/GroupBox.axaml.cs
@@ -1,12 +1,11 @@
 using Avalonia.Controls.Primitives;
 
-namespace GroupBox.Avalonia.Controls
+namespace GroupBox.Avalonia.Controls;
+/// <summary>
+/// <para>A thin wrapper for <see cref="HeaderedContentControl"/> intended for recreations of GroupBox and GroupBox-like Controls.</para>
+/// From Microsoft's documentation: This Control displays a frame around a <i>group</i> of controls with an optional caption.<br/>
+/// </summary>
+public partial class GroupBox : HeaderedContentControl
 {
-    public partial class GroupBox : HeaderedContentControl
-    {
-        public GroupBox()
-        {
-            InitializeComponent();
-        }
-    }
+    public GroupBox() => InitializeComponent();
 }
