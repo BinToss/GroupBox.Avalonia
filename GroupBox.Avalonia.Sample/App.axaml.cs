d50d77eebdd4072edf52eab769609ff365b84ec1
Noah Sherwin <noahrsherwin@gmail.com>
2024-03-10 06:12:26 -0700

    refactor(GroupBox.Avalonia.Sample): move MainWindow content to MainView

--- GroupBox.Avalonia.Sample/App.axaml.cs
+++ GroupBox.Avalonia.Sample/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using GroupBox.Avalonia.Sample.Views;
 using HotAvalonia;
 
 namespace GroupBox.Avalonia.Sample;
