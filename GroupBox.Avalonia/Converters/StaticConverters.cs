4cb5c648dda84c8b99cb518ff69ed1453e8c2408
Noah Sherwin <noahrsherwin@gmail.com>
2024-03-11 16:12:55 -0700

    fix(GroupBox.Avalonia): dynamically adjust spacing
    - Bind Border's Margin and Padding to static resources.
    - Bind their "Top" to GBHeaderText's Bounds Height

--- GroupBox.Avalonia/Converters/StaticConverters.cs
+++ GroupBox.Avalonia/Converters/StaticConverters.cs
@@ -32,5 +32,52 @@ namespace GroupBox.Avalonia.Converters
 
             throw new InvalidCastException();
         });
+        public static FuncValueConverter<OperatingSystem?, CornerRadius> OSBorderCornerRadius { get; } = new((OperatingSystem? osVersion) =>
+            new(osVersion?.Platform switch
+            {
+                PlatformID.Win32NT => osVersion.Version.Major switch
+                {
+                    /* check radius via Windows' presentationsettings.exe */
+
+                    // XP and older. XP may have used 3, but older ones were probably 0.
+                    5 => 0,
+                    6 => osVersion.Version.Minor switch
+                    {
+                        // Vista, 7 are 3
+                        0 or 1 => 3,
+                        // 8, 8.1 are...unknown
+                        _ => 0,
+                    },
+                    // 10 and later. My copies of Win11 use 0.
+                    10 => osVersion.Version.Build switch
+                    {
+                        var win11 when win11 >= 22000 => 0,
+                        _ => 0,
+                    },
+                    _ => 0,
+                },
+                _ => 0
+            }));
+
+        public static FuncMultiValueConverter<object?, Thickness> ConvertDoublesToThickness { get; } = new(src =>
+        {
+            List<double> doubles = new();
+            foreach (var nd in src)
+            {
+                if (nd is double d)
+                    doubles.Add(d);
+                else throw new InvalidCastException();
+            }
+            return doubles.Count switch
+            {
+                1 => new Thickness(doubles[0]),
+                2 => new Thickness(doubles[0], doubles[1]),
+                4 => new Thickness(doubles[0], doubles[1], doubles[2], doubles[3]),
+                _ => throw new IndexOutOfRangeException("Source data must be doubles. Thickness' constructors only allow for 1, 2, or 4 arguments"),
+            };
+        });
+
+        public static FuncValueConverter<Rect?, double> BoundsToHeaderHeightDividedByTwo { get; } = new(rect =>
+        rect.HasValue ? rect.Value.Height / 2 : throw new InvalidCastException());
     }
 }
