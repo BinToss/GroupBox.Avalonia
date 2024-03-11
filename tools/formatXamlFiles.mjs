3bc9ec00a33490a8c2ef8b0f6e87536d76c03a8a
Noah Sherwin <noahrsherwin@gmail.com>
2024-03-11 13:33:23 -0700

    chore: enclose file paths with double-quotes in xstyler script

--- tools/formatXamlFiles.mjs
+++ tools/formatXamlFiles.mjs
@@ -1,7 +1,6 @@
 import { exec } from "node:child_process";
 import * as fs from "node:fs/promises"
 import path, { dirname } from "node:path";
-import { exit } from "node:process";
 import { fileURLToPath } from "node:url";
 
 process.chdir(dirname(fileURLToPath(import.meta.url)) + '/..');
@@ -18,7 +17,7 @@ Promise.all(projNames
         function (v) {
             // must be comma-separated!
             const axamlFiles = v.flat().filter(file => file.endsWith('.axaml')).join(',');
-            const args = ['xstyler', '-c', './Settings.XamlStyler', '-f', axamlFiles, ...process.argv.slice(2)];
+            const args = ['xstyler', '-c', './Settings.XamlStyler', '-f', `"${axamlFiles}"`, ...process.argv.slice(2)];
             const xstyler = exec(`dotnet ${args.join(' ')}`);
             xstyler.stdout.on('data', chunk => process.stdout.write(chunk));
             xstyler.stderr.on('data', chunk => process.stderr.write(chunk));
