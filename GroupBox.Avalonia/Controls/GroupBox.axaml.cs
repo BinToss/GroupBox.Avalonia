312bad4989b43b83f2abdb5e86c0461579a7014e
Noah Sherwin <noahrsherwin@gmail.com>
2024-03-16 04:26:39 -0700

    refactor!: convert ClassicTheme, ModernTheme styles to ControlThemes
    - rename ClassicTheme to GroupBoxClassic
    - rename ModernTheme to GroupBoxBasic
    - add themes to GroupBox StaticResources
    - default to GroupBoxBasic
    - allow switching to GroupBoxClassic by setting the GroupBox's Theme.
      - `Theme="{StaticResource GroupBoxClassic}"`
      - All GroupBoxes have these resource.
    
    BREAKING CHANGES:
    GroupBox styles are now ControlThemes.
    All GroupBoxes initialize with the Basic theme (formerly Modern) applied.
    Use Classic via `Theme="{StaticResource GroupBoxClassic"` in XAML.

--- GroupBox.Avalonia/Controls/GroupBox.axaml.cs
+++ GroupBox.Avalonia/Controls/GroupBox.axaml.cs
@@ -5,6 +5,10 @@ namespace GroupBox.Avalonia.Controls;
 /// <para>A thin wrapper for <see cref="HeaderedContentControl"/> intended for recreations of GroupBox and GroupBox-like Controls.</para>
 /// From Microsoft's documentation: This Control displays a frame around a <i>group</i> of controls with an optional caption.<br/>
 /// </summary>
+/// <remarks>
+///     If you're looking for that win32/WinForms/Aero look, set the GroupBox's Theme to {StaticResource GroupBoxClassic}.<br/>
+///     If you haven't already, you may want to consider reading the <see href="https://web.archive.org/web/20221208093501/https://learn.microsoft.com/en-us/windows/win32/uxguide/ctrl-group-boxes">Windows 7 design guidelines</see>. A GroupBox may be incorrect for the intended content or layout.
+/// </remarks>
 public partial class GroupBox : HeaderedContentControl
 {
     public GroupBox() => InitializeComponent();
