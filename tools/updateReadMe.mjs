90360f465372d02d71dcabc3e9333cb1be08bd33
Noah Sherwin <noahrsherwin@gmail.com>
2024-04-05 19:40:19 -0700

    ci(tools): refactor "updateVersionInAvaloniaBadge"

---
+++ tools/updateReadMe.mjs
@@ -0,0 +1,27 @@
+import { execFileSync } from "child_process";
+import { readFileSync, writeFileSync } from 'fs'
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+// this file's directory
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+function updateVersionInAvaloniaBadge() {
+
+    const dotnet = "dotnet";
+    const args = ['msbuild', './GroupBox.Avalonia/GroupBox.Avalonia.csproj', '-getItem:PackageReference'];
+    const subprocess = execFileSync(dotnet, args, { encoding: 'utf8', stdio: "pipe", });
+
+    const out = subprocess;
+    // console.log('dotnet output: ' + out);
+    // console.log(JSON.parse(out));
+    const avaloniaVersion = JSON.parse(out).Items.PackageReference.find(v => v.Identity === 'Avalonia').Version.toString();
+    const pattern = RegExp('(?<=\\[!\\[avalonia]\\(https:\\/\\/img\\.shields\\.io\\/badge\\/avalonia-v)\\d+\\.\\d+\\.\\d+');
+    const readmePath = join(dirname(__dirname), "README.md");
+    const readMeBuffer = readFileSync(readmePath, { encoding: 'utf8' });
+    //console.log(readMeBuffer);
+    //console.log(readMeBuffer.replace(pattern, avaloniaVersion));
+    writeFileSync(readmePath, readMeBuffer.replace(pattern, avaloniaVersion), { encoding: "utf8" });
+}
+
+updateVersionInAvaloniaBadge();
