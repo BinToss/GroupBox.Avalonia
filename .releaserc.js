da17df0692fdd33109af9240e177d88fd6abb181
Noah Sherwin <noahrsherwin@gmail.com>
2025-08-05 15:54:48 -0700

    ci: remove step ".NET - Pack n' Push NuPkg" in favor of NugetRegistryInfo instances

--- .releaserc.js
+++ .releaserc.js
@@ -1,11 +1,32 @@
+//@ts-check
+import {
+  GithubNugetRegistryInfo,
+  MSBuildProject,
+  NugetRegistryInfo
+} from '@halospv3/hce.shared-config/dotnet';
 import { getConfig } from '@halospv3/hce.shared-config/semanticReleaseConfigDotnet';
+import { inspect } from 'node:util';
 
-const projectsToPublish = [
+/** @type {MSBuildProject[]} */
+const projectsToPublish = await MSBuildProject.PackableProjectsToMSBuildProjects([
   './GroupBox.Avalonia/GroupBox.Avalonia.csproj',
-  './GroupBox.Avalonia.Sample/GroupBox.Avalonia.Sample.csproj'];
-const projectsToPackAndPush = [
-  './GroupBox.Avalonia/GroupBox.Avalonia.csproj'
-];
-const config = await getConfig(projectsToPublish, projectsToPackAndPush);
+  './GroupBox.Avalonia.Sample/GroupBox.Avalonia.Sample.csproj'
+]).then(promises => Promise.all(promises));
+
+const mainProject = projectsToPublish[0];
+if (!mainProject)
+  throw new Error('GroupBox.Avalonia.csproj was not in the evaluated projects array!');
+
+const opts = { project: mainProject };
+
+const config = await getConfig(
+  projectsToPublish,
+  [
+    new GithubNugetRegistryInfo(opts),
+    new NugetRegistryInfo(opts),
+  ]
+);
+
+console.log(inspect(config, { depth: Infinity }));
 
 export default config;
