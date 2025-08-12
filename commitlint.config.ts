653f28339d4141f5a0f23eefe9201be8adcbb5ea
Noah Sherwin <noahrsherwin@gmail.com>
2025-08-12 15:53:56 -0700

    chore(commitlint): add scopes commitlint, deps, deps-dev, release, sample

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,2 +1,18 @@
-import hceShared from "@halospv3/hce.shared-config/commitlintConfig"
+import hceShared from "@halospv3/hce.shared-config/commitlintConfig";
+
+const scopes = {
+  commitlint: 'Affects "./commitlint.config.ts".',
+  deps: 'Affects any runtime dependencies.',
+  'deps-dev': 'Affects dependencies not included at runtime.',
+  release: 'Only for commits made by @semantic-release/git.',
+  sample: 'Affects "./src/GroupBox.Avalonia.Sample/**/*".'
+};
+
+hceShared.rules["scope-enum"] = [
+  2,
+  'always',
+  Reflect.ownKeys(scopes)
+    .map(key => String(key)) as (keyof typeof scopes)[]
+];
+
 export default hceShared;
