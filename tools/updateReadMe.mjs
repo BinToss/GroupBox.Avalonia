d96cc8355cdecd14af2a8f17318a98ee6afd0176
Noah Sherwin <noahrsherwin@gmail.com>
2025-11-02 23:16:56 -0800

    chore: add null checks, types, missing semi-colon in updateReadMe.mjs

--- tools/updateReadMe.mjs
+++ tools/updateReadMe.mjs
@@ -1,6 +1,6 @@
 //@ts-check
 import { execFileSync } from "child_process";
-import { readFileSync, writeFileSync } from 'fs'
+import { readFileSync, writeFileSync } from 'fs';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -16,13 +16,15 @@ function updateVersionInAvaloniaBadge() {
     const out = subprocess;
     // console.log('dotnet output: ' + out);
     // console.log(JSON.parse(out));
-    const avaloniaVersion = JSON.parse(out).Items.PackageReference.find(v => v.Identity === 'Avalonia').Version.toString();
+    /** @type {string | undefined} */
+    const avaloniaVersion = JSON.parse(out)?.Items?.PackageReference?.find((/** @type {{ Identity: string; }} */ v) => v.Identity === 'Avalonia').Version?.toString();
     const pattern = RegExp('(?<=\\[!\\[avalonia]\\(https:\\/\\/img\\.shields\\.io\\/badge\\/avalonia-v)\\d+\\.\\d+\\.\\d+');
     const readmePath = join(dirname(__dirname), "README.md");
     const readMeBuffer = readFileSync(readmePath, { encoding: 'utf8' });
     //console.log(readMeBuffer);
     //console.log(readMeBuffer.replace(pattern, avaloniaVersion));
-    writeFileSync(readmePath, readMeBuffer.replace(pattern, avaloniaVersion), { encoding: "utf8" });
+    if (avaloniaVersion)
+        writeFileSync(readmePath, readMeBuffer.replace(pattern, avaloniaVersion), { encoding: "utf8" });
 }
 
 updateVersionInAvaloniaBadge();
